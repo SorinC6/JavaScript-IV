2ef55b2890b664e06e5efcb5b88731e4fccfd5b5
Sorin Chis <kish.sorin@yahoo.com>
2019-01-24 14:22:02 +0200

    Person class added

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,23 @@
 // CODE here for your Lambda Classes
+
+// function Person(personAttributes)  {
+//     this.name = personAttributes.name;
+//     this.age = personAttributes.age;
+//     this.location = personAttributes.location;
+//   }
+
+class Person {
+	constructor(personAtributes) {
+		this.name = personAtributes.name;
+		this.age = personAtributes.age;
+		this.location = personAtributes.location;
+	}
+}
+
+const fred = new Person({
+	name: "Fred",
+	age: 37,
+	location: "Bedrock"
+});
+
+console.log(fred);
