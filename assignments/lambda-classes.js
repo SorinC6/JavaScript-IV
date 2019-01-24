7d897095d3204f2c674f2ef2471d6b625fe89df5
Sorin Chis <kish.sorin@yahoo.com>
2019-01-24 14:49:56 +0200

    Person class and Instructor class added with methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,14 +10,45 @@ class Person {
 	constructor(personAtributes) {
 		this.name = personAtributes.name;
 		this.age = personAtributes.age;
-		this.location = personAtributes.location;
-	}
+        this.location = personAtributes.location;
+        this.gender=personAtributes.gender;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I'm from ${this.location}`;
+    }
 }
 
-const fred = new Person({
-	name: "Fred",
-	age: 37,
-	location: "Bedrock"
-});
+// const fred = new Person({
+// 	name: "Fred",
+// 	age: 37,
+// 	location: "Bedrock"
+// });
 
-console.log(fred);
+//console.log(fred.speak());
+
+class Instructor extends Person{
+    constructor(instructorAtributes){
+        super(instructorAtributes);
+        this.specialty=instructorAtributes.specialty;
+        this.favLanguage=instructorAtributes.favLanguage;
+        this.catchPhrase=instructorAtributes.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student,subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`)
+    }
+}
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  console.log(fred.demo('redux'));
